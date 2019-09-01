63c82f93e8cf939f8e94bbb4c38a066bb95996a7
fummicc1 <fumiya.tennis1@gmail.com>
2019-09-01 18:38:05 +0900

    サイレントプッシュ実装

--- LocationShareAppWithAPNS/AppDelegate.swift
+++ LocationShareAppWithAPNS/AppDelegate.swift
@@ -1,46 +1,42 @@
-//
-//  AppDelegate.swift
-//  LocationShareAppWithAPNS
-//
-//  Created by Fumiya Tanaka on 2019/09/01.
-//  Copyright © 2019 Fumiya Tanaka. All rights reserved.
-//
-
 import UIKit
+import UserNotifications
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
 
     var window: UIWindow?
-
+    var deviceToken: String = ""
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        application.registerForRemoteNotifications()
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound], completionHandler: { (_, _) in })
+        UNUserNotificationCenter.current().delegate = self
         return true
     }
-
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+    
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        self.deviceToken = deviceToken.map { String(format: "%02.2hhx", $0) }.joined()
+        print(self.deviceToken)
     }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.alert, .sound])
     }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+    
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        
+        let latitude = Double.random(in: 0...90)
+        let longitude = Double.random(in: 0...180)
+        
+        let url = URL(string: "https://11443b5e.ngrok.io/location?latitude=\(latitude)&longitude=\(longitude)")!
+        
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if error == nil {
+                completionHandler(.newData)
+            } else {
+                completionHandler(.failed)
+            }
+        }.resume()
     }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
-
 }
 
--- LocationShareAppWithAPNS/ViewController.swift
+++ LocationShareAppWithAPNS/ViewController.swift
@@ -1,20 +1,10 @@
-//
-//  ViewController.swift
-//  LocationShareAppWithAPNS
-//
-//  Created by Fumiya Tanaka on 2019/09/01.
-//  Copyright © 2019 Fumiya Tanaka. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        super.viewDidLoad()        
     }
-
-
+    
 }
 
